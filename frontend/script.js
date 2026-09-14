7acbe78233eacf21a560c1195b8c05c2214b29cf
zizdkhili <azizayg01@gmail.com>
2026-09-14 04:02:39 +0100

    Final fix: use image_url in script.js

--- frontend/script.js
+++ frontend/script.js
@@ -17,7 +17,7 @@ fetch('/api/sites')
             // التغيير هوني: زدنا التصويرة في الـ Popup وكبرنا العرض شوية (220px)
             marker.bindPopup(`
                 <div style="text-align: left; width: 220px;">
-                    <img src="${site.image}" width="200">
+                    <img src="${site.image_url}" width="200">
                     <h3 style="margin-bottom: 5px; color: #d32f2f; margin-top: 0;">${site.name}</h3>
                     <p style="margin-top: 0; font-size: 13px;"><b>Category:</b> ${site.category}</p>
                     <p style="font-size: 14px;">${site.description}</p>
